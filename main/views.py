5ebe4c719132a97a36e11f39ae6981f71c890c1d
BKsoni <bhavyasoni24.bs@gmail.com>
2023-08-17 17:58:48 +0530

    adding top gainers, losers

--- main/views.py
+++ main/views.py
@@ -1,10 +1,20 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 import yfinance as yf
+from decouple import config
+import requests
+
+API_KEY = config("ALPHA_VANTAGE_API_KEY")
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    url = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=XWN6DUC5D5WU2L9Q'
+    r = requests.get(url)
+    data = r.json()
+    top_gainers = [ data['top_gainers'][i] for i in range(10) ]
+    top_losers = [ data['top_losers'][i] for i in range(10) ]
+    return render(request, 'index.html', {'title': 'Home', 'top_gainers': top_gainers, 'top_losers': top_losers})
 
 def about(request):
     return render(request, 'about.html')
